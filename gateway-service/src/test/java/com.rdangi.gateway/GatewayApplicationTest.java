f1c745d3c5fdd7e7221b0cf9db5af11dc217099a
Amiya Sahu <amiya053@gmail.com>
2020-03-15 20:40:00 -0400

    Update spring boot and added tests for the gateway service.
    The gateway service test will be enhanced based on the functionality

--- gateway-service/src/test/java/com.rdangi.gateway/GatewayApplicationTest.java
+++ gateway-service/src/test/java/com.rdangi.gateway/GatewayApplicationTest.java
@@ -1,18 +1,49 @@
 package com.rdangi.gateway;
 
-
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(
+        webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
+        properties = {"gateway.target.url.hello-service=http://localhost:${wiremock.server.port}"})
+@AutoConfigureWireMock(port = 0)
 public class GatewayApplicationTest {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Autowired
+    private WebTestClient webClient;
+
+    @Test
+    public void testGatewayService() throws Exception {
+        stubFor(get(urlEqualTo("/hello"))
+                .willReturn(aResponse()
+                        .withBody("{\"content\":\"Hello World!\"}")
+                        .withHeader("Content-Type", "application/json")));
+
+        stubFor(get(urlEqualTo("/hello?name=John"))
+                .willReturn(aResponse()
+                        .withBody("{\"content\":\"Hello John!\"}")
+                        .withHeader("Content-Type", "application/json")));
+
+        webClient.get().uri("/hello")
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody()
+                .jsonPath("$.content").isEqualTo("Hello World!");
+
+        webClient.get().uri("/hello?name=John")
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody()
+                .jsonPath("$.content").isEqualTo("Hello John!");
+    }
 
 }
-
