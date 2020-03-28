a02b5b71a95e50f45dfd22c6f2d8eb6254cd06ae
Amiya Sahu <amiya053@gmail.com>
2020-03-27 21:20:09 -0400

    Updated tests to avoid un-necessary exceptions

--- spring-boot-admin/src/test/java/com/rdangi/springbootadmin/SpringBootAdminApplicationTests.java
+++ spring-boot-admin/src/test/java/com/rdangi/springbootadmin/SpringBootAdminApplicationTests.java
@@ -2,8 +2,10 @@ package com.rdangi.springbootadmin;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(locations = {"classpath:disable-eureka-cloud-config.properties"})
 class SpringBootAdminApplicationTests {
 
 	@Test
