9852c55d47f693bb2cd8df57f9c72c6ed1d81373
Amiya Sahu <amiya053@gmail.com>
2020-03-30 13:30:35 -0400

    Added doc

--- hello-service/src/main/java/com/rdangi/helloservice/config/CacheConfiguration.java
+++ hello-service/src/main/java/com/rdangi/helloservice/config/CacheConfiguration.java
@@ -3,6 +3,9 @@ package com.rdangi.helloservice.config;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Profile;
 
+/**
+ * Cache configuration. This is only enabled while running on docker
+ */
 @Profile("docker")
 @EnableCaching
 public class CacheConfiguration {
